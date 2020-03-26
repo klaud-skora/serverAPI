48075e3984577ce5795d3375d2c18f91f383589f
Klaudia Skóra <klaud.skora@gmail.com>
2020-03-26 13:11:37 +0100

    Make changes for publishing projekt on Heroku

--- client/src/redux/seatsRedux.js
+++ client/src/redux/seatsRedux.js
@@ -34,7 +34,7 @@ export const loadSeatsRequest = () => {
     try {
 
       let res = await axios.get(`${API_URL}/seats`);
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      
       dispatch(loadSeats(res.data));
       dispatch(endRequest({ name: 'LOAD_SEATS' }));
 
@@ -51,7 +51,7 @@ export const addSeatRequest = (seat) => {
     dispatch(startRequest({ name: 'ADD_SEAT' }));
     try {
       let res = await axios.post(`${API_URL}/seats`, seat);
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      
       dispatch(addSeat(res));
       dispatch(endRequest({ name: 'ADD_SEAT' }));
 
