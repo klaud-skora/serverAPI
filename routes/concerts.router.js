0560e59279b3844fd5d6df7f008b7fdd117d8a5c
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-17 10:37:46 +0200

    Add new endpoints for finding concerts

--- routes/concerts.router.js
+++ routes/concerts.router.js
@@ -6,7 +6,15 @@ const ConcertController = require('../controllers/concerts.controller');
 router.get('/concerts', ConcertController.getAll);
   
 router.get('/concerts/:id', ConcertController.getId);
-  
+
+router.get('/concerts/performer/:performer', ConcertController.getPerformer);
+
+router.get('/concerts/genre/:genre', ConcertController.getGenre);
+
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getPrice);
+
+router.get('/concerts/price/day/:day', ConcertController.getConcertByDay);
+
 router.post('/concerts', ConcertController.postDoc);
   
 router.delete('/concerts/:id', ConcertController.deleteId);
